72b37f6374f73416e749a09086316e3f1907d28f
Aur Saraf <aur@sarafconsulting.com>
2022-09-07 20:14:34 +0300

    reverse()

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
+pub fn reverse(s: &str) -> String {
+    s.chars().into_iter().rev().collect()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(reverse("hello"), "olleh");
+    }
+
+    #[test]
+    fn edge_cases() {
+        assert_eq!(reverse(""), "");
+        assert_eq!(
+            reverse("123456789012345678901234567890"),
+            "098765432109876543210987654321"
+        );
     }
 }
