8d9bee9d64f4d285772648a96a8ed1d2effdf94a
Aur Saraf <aur@sarafconsulting.com>
2022-09-07 21:15:47 +0300

    from python

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,15 +1,14 @@
-
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn reverse(s: &str) -> PyResult<String> {
+    Ok(rust_everywhere::reverse(s))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn python(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn rust_everywhere_python(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(reverse, m)?)?;
     Ok(())
 }
